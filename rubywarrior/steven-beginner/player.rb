9b03780f18700d2e7665581c1f20b4e0d26e6f73
Steven Barragan <me@steven.mx>
2013-04-05 00:26:23 -0600

    Improving leven 4

--- rubywarrior/steven-beginner/player.rb
+++ rubywarrior/steven-beginner/player.rb
@@ -17,7 +17,8 @@ class Player
       warrior.attack!
 
     elsif self.been_attached?(warrior)
-      if warrior.health < 10
+      
+      if warrior.health < 10 && self.enemy_distance(warrior) > 2
         warrior.walk! :backward
       else
         warrior.walk!
@@ -29,7 +30,11 @@ class Player
         warrior.shoot!
         @shoot += 1
       else
-        warrior.walk!
+        if warrior.health < 10
+          warrior.rest!
+        else
+          warrior.walk!
+        end
         @shoot = 0
       end
 
@@ -45,32 +50,20 @@ class Player
 
   end
 
+  def enemy_distance(warrior)
+    count = 0
+    
+    warrior.look.each{|feel|
+      return count if feel.enemy?
+      count += 1
+    }
+
+  end
+
   def look_stairs?(warrior)
     warrior.look.each { |feel| 
       return true if feel.stairs?
     }
-  end
-
-  def look_do!(warrior)
-
-    enemy = false
-    captive = true
-    stairs = true
-
-    warrior.look.each{ |feel| 
-
-      if feel.enemy?
-        enemy = true
-
-      elsif feel.captive?
-        captive = true
-
-      elsif fell.stairs?
-        stairs = true
-      end
-    }
-
-
   end
 
   def attack_distance?(warrior)
