1476434bd2fcff0d693005cec3b947cbbf2bd38f
Steven Barragan <me@steven.mx>
2013-04-04 20:06:10 -0600

    level 4 pass

--- rubywarrior/steven-beginner/player.rb
+++ rubywarrior/steven-beginner/player.rb
@@ -1,13 +1,31 @@
 class Player
+  
+  def initialize()
+  	@health = 20
+  end
+
   def play_turn(warrior)
   	if warrior.feel.empty?
-  		if warrior.health < 10
-  			warrior.rest!
-  		else
-    		warrior.walk!
-    	end
+  		if self.been_attached?(warrior) || warrior.health > 15
+	    	warrior.walk!
+	    else
+	    	warrior.rest!
+	    end
     else
     	warrior.attack!
+  		
+  	@health = warrior.health
+
+  	puts "Healt #{ @health }"
+
     end
   end
+
+  def been_attached?(warrior)
+  	warrior.health < @health
+  end
+
+
 end
+
+
